ed5b3a38bd51a7554ce2674947809325ae6dad79
emilisav <emikaya@byu.edu>
2024-04-11 12:07:09 -0600

    basic css and routing working

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,93 @@
 import React from "react";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
+import { Login } from "./login/login";
+import { Questions } from "./questions/questions";
+import { Add } from "./add/add";
+import { AuthState } from "./login/authState";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
-export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+function App() {
+  const [userName, setUserName] = React.useState(
+    localStorage.getItem("userName") || ""
+  );
+  const currentAuthState = userName
+    ? AuthState.Authenticated
+    : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
+  return (
+    <BrowserRouter>
+      <div className="body">
+        <header>
+          <h1>Welcome to TalkShow!</h1>
+
+          <nav className="navbar">
+            <menu className="navbar-nav">
+              {authState === AuthState.Authenticated && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="">
+                    Logout
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="questions">
+                    Questions
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="add">
+                    Add Questions
+                  </NavLink>
+                </li>
+              )}
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route
+            path="/questions"
+            element={<Questions userName={userName} />}
+          />
+          <Route path="/add" element={<Add userName={userName} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+
+        <footer>
+          <span className="text-reset">Emily De Graw</span>
+          <a href="https://github.com/Emilisav/startup">GitHub</a>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
 }
+
+function NotFound() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      404: Return to sender. Address unknown.
+    </main>
+  );
+}
+
+export default App;
---
+++ src/questions/questions.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Questions() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>questions displayed here</div>
+    </main>
+  );
+}
